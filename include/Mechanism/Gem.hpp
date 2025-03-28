ef7816fc8903bf3ceb7296ea73a447df63f8d96f
yunchien77 <b812110004@tmu.edu.tw>
2025-03-28 12:40:26 +0800

    update level2 init position & reset game

--- include/Mechanism/Gem.hpp
+++ include/Mechanism/Gem.hpp
@@ -1,35 +1,30 @@
 #ifndef GEM_HPP
 #define GEM_HPP
 
-#include "Interface/IGem.hpp"
-#include "Util/GameObject.hpp"
 #include "Character/Character.hpp"
+#include "Interface/IGem.hpp"
 
-enum class GemType {
-    FIRE,
-    WATER,
-    GREEN
-};
+enum class GemType { FIRE, WATER, GREEN };
 
-class Gem : public IGem, public Util::GameObject{
+class Gem : public IGem {
 public:
-    explicit Gem(GemType type);
+  explicit Gem(GemType type);
 
-    void OnCharacterEnter(Character* character) override;
+  void OnCharacterEnter(Character *character) override;
 
-    const SDL_Rect& getRect() const override;
+  const SDL_Rect &getRect() const override;
 
-    void SetPosition(const glm::vec2& position);
-    GemType GetType() const;
+  void SetPosition(const glm::vec2 &position);
+  GemType GetType() const;
 
-    void SetInitialPosition(const glm::vec2& pos);
-    void Respawn() override;
+  void SetInitialPosition(const glm::vec2 &pos);
+  void Respawn() override;
 
 private:
-    GemType m_Type;
-    bool m_Collected = false;
-    mutable SDL_Rect m_Rect;
-    glm::vec2 m_InitialPosition;
+  GemType m_Type;
+  //   bool m_Collected = false;
+  mutable SDL_Rect m_Rect;
+  glm::vec2 m_InitialPosition;
 };
 
 #endif // GEM_HPP
--- src/Game/AppGamePlay.cpp
+++ src/Game/AppGamePlay.cpp
@@ -189,10 +189,11 @@ bool App::LoadLevelGrid(int levelNumber) {
 
   } break;
 
-  case 2: { // 初始化角色 Fireboy
+  case 2: {
+    // 初始化角色 Fireboy
     if (!m_Fireboy) {
       m_Fireboy = std::make_shared<Fireboy>();
-      glm::vec2 fireboyInitPos = m_GridSystem.CellToGamePosition(35, 5);
+      glm::vec2 fireboyInitPos = m_GridSystem.CellToGamePosition(2, 27);
       m_Fireboy->SetPosition(fireboyInitPos);
       m_Fireboy->SetSpawnPoint(fireboyInitPos);
       m_Root.AddChild(m_Fireboy);
@@ -201,20 +202,20 @@ bool App::LoadLevelGrid(int levelNumber) {
     // 初始化角色 Watergirl
     if (!m_Watergirl) {
       m_Watergirl = std::make_shared<Watergirl>();
-      glm::vec2 watergirlInitPos = m_GridSystem.CellToGamePosition(3, 17);
+      glm::vec2 watergirlInitPos = m_GridSystem.CellToGamePosition(4, 27);
       m_Watergirl->SetPosition(watergirlInitPos);
       m_Watergirl->SetSpawnPoint(watergirlInitPos);
       m_Root.AddChild(m_Watergirl);
     }
 
     // Fireboy 的門
-    glm::vec2 fireboyDoorPos = m_GridSystem.CellToGamePosition(32, 14);
+    glm::vec2 fireboyDoorPos = m_GridSystem.CellToGamePosition(2, 4);
     m_Fireboy_Door->SetPosition(fireboyDoorPos);
     m_Fireboy_Door->SetOpen(false);
     m_Fireboy_Door->SetVisible(true);
 
     // Watergirl 的門
-    glm::vec2 watergirlDoorPos = m_GridSystem.CellToGamePosition(4, 27);
+    glm::vec2 watergirlDoorPos = m_GridSystem.CellToGamePosition(6, 4);
     m_Watergirl_Door->SetPosition(watergirlDoorPos);
     m_Watergirl_Door->SetOpen(false);
     m_Watergirl_Door->SetVisible(true);
